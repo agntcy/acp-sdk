8f684c0d45129635ff2a463656faff694a2cad54
Alessandro Duminuco <aduminuc@cisco.com>
2025-02-26 14:55:07 +0100

    fix: workflow

--- scripts/check-models.sh
+++ scripts/check-models.sh
@@ -3,7 +3,7 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 Cisco and/or its affiliates.
 # SPDX-License-Identifier: Apache-2.0
 
-if [[ $( git status --porcelain ) != "" ]]; then
+if [[ "$( git status --porcelain )" != "" ]]; then
     echo "ERROR: stale SDK models. Run 'make generate_sdk_models' and commit again";
     git status --porcelain
 	exit 1;
