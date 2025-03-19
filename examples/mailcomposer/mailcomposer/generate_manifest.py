d4b55a6d0c345d7c2a296a12c600d25a492bf756
Alessandro Duminuco <aduminuc@cisco.com>
2025-03-19 16:19:38 +0100

    adding mail composer

---
+++ examples/mailcomposer/mailcomposer/generate_manifest.py
@@ -0,0 +1,41 @@
+from state import AgentState, OutputState
+from agntcy_acp.manifest import *
+from pydantic import TypeAdapter
+
+
+manifest = AgentManifest(
+    metadata=AgentMetadata(
+        ref=AgentRef(name="org.agntcy.mailcomposer", version="0.0.1"),
+        description="Offer a chat interface to compose an email for a marketing campaign. Final output is the email that could be used for the campaign"),
+    specs=AgentACPSpec(
+        input=TypeAdapter(AgentState).json_schema(),
+        output=TypeAdapter(OutputState).json_schema(),
+        config=TypeAdapter(OutputState).json_schema(),
+        capabilities=Capabilities(
+            threads=False,
+            callbacks=False,
+            interrupts=False
+        )
+    ),
+    deployment=AgentDeployment(
+        deployment_options=[
+            SourceCodeDeployment(
+                type="source_code",
+                name="source_code_local",
+                url="file://../",
+                framework_config=LangGraphConfig(
+                    framework_type="langgraph",
+                    graph="mailcomposer.mailcomposer:graph"
+                )
+            )
+        ],
+        dependencies=[]
+    )
+)
+
+with open("../deploy/mailcomposer.json", "w") as f:
+    f.write(manifest.model_dump_json(
+        exclude_unset=True,
+        exclude_none=True,
+        indent=2
+    ))
