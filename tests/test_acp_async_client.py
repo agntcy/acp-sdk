a8265eebf8ce022b03a95ffaa1bb299f6cf7a323
Jeff Napper <103025963+jnapper7@users.noreply.github.com>
2025-03-26 13:07:13 +0100

    fix: updated to latest 0.2.1 ACP spec.

--- tests/test_acp_async_client.py
+++ tests/test_acp_async_client.py
@@ -3,7 +3,7 @@
 import datetime
 import io
 from agntcy_acp import AsyncACPClient, ApiResponse, ApiClientConfiguration, AsyncApiClient
-from agntcy_acp.models import RunCreation, RunSearchRequest, Run, RunStatus, RunCreateStateless
+from agntcy_acp.models import RunSearchRequest, RunStateless, RunStatus, RunCreateStateless
 
 class RESTResponse(io.IOBase):
     def __init__(self, status, body) -> None:
@@ -25,9 +25,10 @@ class RESTResponse(io.IOBase):
 async def test_acp_client_runs_api(monkeypatch):
     agent_id = "bogus-agent-id"
     init_run_id = "bugus-run-id"
-    run_create = RunCreation(agent_id=agent_id)
+    run_create = RunCreateStateless(agent_id=agent_id)
+    config = ApiClientConfiguration(retries=2, api_key={"x-api-key": "bogus-api-key"})
 
-    api_client = AsyncApiClient(ApiClientConfiguration(retries=2, api_key={"x-api-key": "bogus-api-key"}))
+    api_client = AsyncApiClient(config)
     # Make sure apis return data
     async def mock_call_api(
         method,
@@ -46,7 +47,7 @@ run_id: 1234-5678-90123
 
     # Make sure data is deserialized
     def mock_response_deserialize(response_data, response_types_map = None):
-        run = Run(
+        run = RunStateless(
             run_id=init_run_id, 
             agent_id=agent_id, 
             creation=run_create,
@@ -54,28 +55,30 @@ run_id: 1234-5678-90123
             created_at=datetime.datetime.now(),
             updated_at=datetime.datetime.now(),
         )
-        return ApiResponse[Run](status_code=200, data=run, raw_data=run.model_dump_json(exclude_unset=True).encode())
+        return ApiResponse[RunStateless](status_code=200, data=run, raw_data=run.model_dump_json(exclude_unset=True).encode())
     monkeypatch.setattr(api_client, "response_deserialize", mock_response_deserialize)
-    client = AsyncACPClient(api_client)
 
-    response = await client.create_stateless_run(run_create_stateless=RunCreateStateless(agent_id=agent_id))
-    assert response is not None
-    run_id = response.run_id
+    async with api_client:
+        client = AsyncACPClient(api_client)
 
-    response = await client.get_stateless_run(run_id)
-    assert response is not None
+        response = await client.create_stateless_run(run_create_stateless=RunCreateStateless(agent_id=agent_id))
+        assert response is not None
+        run_id = response.run_id
 
-    response = await client.wait_for_stateless_run_output(run_id)
-    assert response is not None
+        response = await client.get_stateless_run(run_id)
+        assert response is not None
 
-    response = await client.stream_stateless_run_output(run_id)
-    assert response is not None
+        response = await client.wait_for_stateless_run_output(run_id)
+        assert response is not None
 
-    response = await client.resume_stateless_run(run_id, {})
-    assert response is not None
+        response = await client.stream_stateless_run_output(run_id)
+        assert response is not None
 
-    response = await client.search_stateless_runs(RunSearchRequest(agent_id=agent_id))
-    assert response is not None
+        response = await client.resume_stateless_run(run_id, {})
+        assert response is not None
 
-    response = await client.delete_stateless_run(run_id)
-    assert response is not None
+        response = await client.search_stateless_runs(RunSearchRequest(agent_id=agent_id))
+        assert response is not None
+
+        response = await client.delete_stateless_run(run_id)
+        assert response is not None
--- tests/test_acp_client.py
+++ tests/test_acp_client.py
@@ -3,7 +3,7 @@
 import datetime
 import io
 from agntcy_acp import ACPClient, ApiResponse, ApiClientConfiguration, ApiClient
-from agntcy_acp.models import RunCreation, RunSearchRequest, Run, RunStatus, RunCreateStateless
+from agntcy_acp.models import RunSearchRequest, RunStateless, RunStatus, RunCreateStateless
 
 class RESTResponse(io.IOBase):
     def __init__(self, status, body) -> None:
@@ -25,9 +25,10 @@ class RESTResponse(io.IOBase):
 def test_acp_client_runs_api(monkeypatch):
     agent_id = "bogus-agent-id"
     init_run_id = "bugus-run-id"
-    run_create = RunCreation(agent_id=agent_id)
+    run_create = RunCreateStateless(agent_id=agent_id)
+    config = ApiClientConfiguration(retries=2, api_key={"x-api-key": "bogus-api-key"})
 
-    api_client = ApiClient(ApiClientConfiguration(retries=2, api_key={"x-api-key": "bogus-api-key"}))
+    api_client = ApiClient(config)
     # Make sure apis return data
     def mock_call_api(
         method,
@@ -46,7 +47,7 @@ run_id: 1234-5678-90123
 
     # Make sure data is deserialized
     def mock_response_deserialize(response_data, response_types_map = None):
-        run = Run(
+        run = RunStateless(
             run_id=init_run_id, 
             agent_id=agent_id, 
             creation=run_create,
@@ -54,28 +55,30 @@ run_id: 1234-5678-90123
             created_at=datetime.datetime.now(),
             updated_at=datetime.datetime.now(),
         )
-        return ApiResponse[Run](status_code=200, data=run, raw_data=run.model_dump_json(exclude_unset=True).encode())
+        return ApiResponse[RunStateless](status_code=200, data=run, raw_data=run.model_dump_json(exclude_unset=True).encode())
     monkeypatch.setattr(api_client, "response_deserialize", mock_response_deserialize)
-    client = ACPClient(api_client)
 
-    response = client.create_stateless_run(run_create_stateless=RunCreateStateless(agent_id=agent_id))
-    assert response is not None
-    run_id = response.run_id
+    with api_client:
+        client = ACPClient(api_client)
 
-    response = client.get_stateless_run(run_id)
-    assert response is not None
+        response = client.create_stateless_run(run_create_stateless=RunCreateStateless(agent_id=agent_id))
+        assert response is not None
+        run_id = response.run_id
 
-    response = client.wait_for_stateless_run_output(run_id)
-    assert response is not None
+        response = client.get_stateless_run(run_id)
+        assert response is not None
 
-    response = client.stream_stateless_run_output(run_id)
-    assert response is not None
+        response = client.wait_for_stateless_run_output(run_id)
+        assert response is not None
 
-    response = client.resume_stateless_run(run_id, {})
-    assert response is not None
+        response = client.stream_stateless_run_output(run_id)
+        assert response is not None
 
-    response = client.search_stateless_runs(RunSearchRequest(agent_id=agent_id))
-    assert response is not None
+        response = client.resume_stateless_run(run_id, {})
+        assert response is not None
 
-    response = client.delete_stateless_run(run_id)
-    assert response is not None
+        response = client.search_stateless_runs(RunSearchRequest(agent_id=agent_id))
+        assert response is not None
+
+        response = client.delete_stateless_run(run_id)
+        assert response is not None
